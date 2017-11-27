b573e94d8f1d6df8cd903c69abec161095d155b9
Davide Allavena <davide.allavena@gmail.com>
2017-11-27 14:34:14 +0100

    Init

---
+++ scraper.py
@@ -0,0 +1,30 @@
+import requests
+import re
+from time import sleep
+
+save_dir = 'docs/'
+domain = 'http://eur-lex.europa.eu/'
+page_base_url = 'http://eur-lex.europa.eu/search.html?instInvStatus=ALL&qid=1407932788225&DTS_DOM=EU_LAW&type=advanced&lang=en&SUBDOM_INIT=EU_CASE_LAW&DTS_SUBDOM=EU_CASE_LAW&page='
+hook_text = './legal-content/EN/TXT/HTML/?'
+html_link_re = re.compile(r'./legal-content/EN/TXT/HTML/[^"]*')
+
+for i in range(1,10000):
+  response = requests.get(page_base_url + str(i))
+  for path in  re.findall(html_link_re, response.text):
+    sleep(1)
+    continua = 1
+
+    url = domain + path[2:]
+    print "Downloading " + url
+
+    while continua:
+      try:
+        page = requests.get(url)
+        continua = 0
+      except:
+        continua = 1
+
+    celex = re.search("uri=([A-Z0-9:\(\)]+)", path).group(1)
+
+    with open(save_dir + celex, 'w') as page_file: 
+      page_file.write(page.text.encode('utf-8'))
