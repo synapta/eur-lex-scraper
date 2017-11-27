a75d9eb40e93a1c153fb3ecd31a1abd60830e25f
Davide Allavena <davide.allavena@gmail.com>
2017-11-27 14:57:55 +0100

    Added directory creation

--- scraper.py
+++ scraper.py
@@ -1,12 +1,16 @@
 import requests
 import re
 from time import sleep
+import os
 
 save_dir = 'docs/'
 domain = 'http://eur-lex.europa.eu/'
 page_base_url = 'http://eur-lex.europa.eu/search.html?instInvStatus=ALL&qid=1407932788225&DTS_DOM=EU_LAW&type=advanced&lang=en&SUBDOM_INIT=EU_CASE_LAW&DTS_SUBDOM=EU_CASE_LAW&page='
 hook_text = './legal-content/EN/TXT/HTML/?'
 html_link_re = re.compile(r'./legal-content/EN/TXT/HTML/[^"]*')
+
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
 
 for i in range(1,10000):
   response = requests.get(page_base_url + str(i))
